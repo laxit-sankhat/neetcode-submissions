95814c52b091bf52c8c194ec97584333b6031e8b
laxit-sankhat <laxitsankhat@gmail.com>
2026-06-19 10:41:09 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        two_houses_back = 0
+        one_house_back = 0
+
+        for money in nums:
+            current = max(
+                one_house_back,         
+                money + two_houses_back  
+            )
+
+            two_houses_back = one_house_back
+            one_house_back = current
+
+        return one_house_back
